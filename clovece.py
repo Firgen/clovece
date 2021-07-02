89e22c2d38fa57a531d7f1e36a29494087bc29dc
Firgen <filip.frgelec@disroot.org>
2021-07-02 19:52:43 +0200

    fixed colors of figures

--- clovece.py
+++ clovece.py
@@ -19,11 +19,11 @@ pole = list("        O O O O O        \n" +\
 # farby
 RESET = "\033[0m"
 farby = ["RED", "GREEN", "BLUE", "YELLOW"]
-farby2 = {"RED": "\033[1;31m",
-          "GREEN": "\033[1;32m",
-          "BLUE": "\033[1;34m",
-          "YELLOW": "\033[1;33m",
-          "CYAN": "\033[1;36m"}
+farby2 = {"RED": "\033[1;91m",
+          "GREEN": "\033[1;92m",
+          "BLUE": "\033[1;94m",
+          "YELLOW": "\033[1;93m",
+          "CYAN": "\033[1;96m"}
 
 # všetky políčka tmavšie
 for i in range(len(pole)):
